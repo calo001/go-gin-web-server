61a6e10ff61d7eb08a16ed7be336e9f283496937
Carlos Lopez <calo_lrc@hotmail.com>
2019-07-06 16:06:16 -0500

    add other dependencies

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 go get -u github.com/gin-gonic/gin
 go get -d -v github.com/dustin/go-broadcast/...
 go get -d -v github.com/manucorporat/stats/...
+go get -u github.com/appleboy/gin-jwt
+go get -u github.com/appleboy/gin-jwt/v2
+go get -u github.com/lib/pq
+go get -u github.com/lib/pq/hstore
 go build -o ./app main.go rooms.go routes.go stats.go
